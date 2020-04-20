9ae95b1fda0fafca511c742686cb523471f1de0f
shubhamverma6594 <47112198+shubhamverma6594@users.noreply.github.com>
2020-04-20 16:22:59 +0530

    Update run_api.py

--- run_api.py
+++ run_api.py
@@ -49,5 +49,4 @@ start_rabbit()
 
 if __name__ == '__main__':
     host = os.environ.get('IP', '127.0.0.1')
-    port = int(os.environ.get('PORT', 80))
-    app.run(host=host, port=port, debug=False)
+    app.run(host=host, debug=False)
